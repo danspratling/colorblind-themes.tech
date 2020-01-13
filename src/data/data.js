243bc51b6385bb19b7b84274f5407846dab8b5cf
Dan Spratling <danspratling@hotmail.co.uk>
2020-01-13 23:34:02 +0000

    Adds more data

--- src/data/data.js
+++ src/data/data.js
@@ -32,9 +32,26 @@ const themes = [
         Protanopia, Deuteranopia, Tritanopia, Monochromatic
       Descriptions can be found here http://www.colourblindawareness.org/colour-blindness/types-of-colour-blindness/
     */
-    colors: [],
+    colors: ["Protanopia"],
 
     //Editors the theme supports. Editors can be found in editors.js
+    editors: [editors.vscode],
+  },
+  {
+    name: "Night Owl/Light Owl",
+    description:
+      "A Visual Studio Code theme for the night owls out there. Fine-tuned for those of us who like to code late into the night. Color choices have taken into consideration what is accessible to people with colorblindness and in low-light circumstances. Decisions were also based on meaningful contrast for reading comprehension and for optimal razzle dazzle.",
+    logo:
+      "https://sdras.gallerycdn.vsassets.io/extensions/sdras/night-owl/1.1.3/1555060598709/Microsoft.VisualStudio.Services.Icons.Default",
+    images: [
+      "https://github.com/sdras/night-owl-vscode-theme/raw/master/first-screen.jpg",
+      "https://github.com/sdras/night-owl-vscode-theme/raw/master/three-dark.jpg",
+      "https://github.com/sdras/night-owl-vscode-theme/raw/master/light-owl-full.jpg",
+      "https://github.com/sdras/night-owl-vscode-theme/raw/master/three-light.jpg",
+    ],
+    repo: "https://github.com/sdras/night-owl-vscode-theme",
+    url: "",
+    colors: ["Protanopia", "Deuteranopia", "Triatanopia", "Monochromatic"],
     editors: [editors.vscode],
   },
 ]
